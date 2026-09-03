a4a639fb2ded2090f58975d7e9b2378557d29baf
SYED RAFAY <137159230+syedabdurrafay@users.noreply.github.com>
2026-09-03 17:29:36 +0500

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: "export",
+
+  basePath: "/s-and-a-attire-",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
